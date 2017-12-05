f3cd1d7cbf22440b3ca587496aabd7b9b8411a9f
cormacio100 <cormac.music@gmail.com>
2017-12-05 23:13:43 +0000

    updated routing

--- todo/urls.py
+++ todo/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     #   we need to invoke the as_view method on the TodoView class
     #   Any view that inherits from the base View object will have the as_view method
     #   This will allow Django to use the class-based view as a standard function-based view.
-    url(r'^$',TodoView.as_view())
+    url(r'^$',TodoView.as_view(), name='todo_all')
 ]
